ebbab5a174922f330bb83a60429a7a994a72c43e
iblacksand <chess4j@gmail.com>
2017-07-07 13:25:54 -0600

    changes method and runs on all urls

--- PadlockCreateAnchor.js
+++ PadlockCreateAnchor.js
@@ -4,7 +4,10 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://25.74.252.196/dashboard/
+// @match        https://*
+// @match        http://*
+// @include     https://*
+// @include     http://*
 // @grant        none
 // ==/UserScript==
 
@@ -12,8 +15,27 @@
 
 (function() {
     'use strict';
-    var ancho = document.createElement('a');
-    ancho.setAttribute('href',"?datareset=1");
-    document.getElementsByClassName("paired-devices")[0].appendChild(ancho);
-    ancho.click();
+    //if title is padlock
+    if (document.title.toLowerCase().indexOf("padlock") === -1) return;
+    //fi title is padlock
+
+    //if sheets contain dashboard
+    var close = true;
+    var sheets = document.styleSheets;
+    for (var x = 0, len = sheets.length; x < len; x++) {
+        if (sheets[x].href && sheets[x].href.toLowerCase().indexOf("dashboard")!==-1) {
+            close = false;
+        }
+    }
+    if (close) return;
+    //fi sheets contain dashboard
+
+    // if pairing
+    var parameters = location.search.substring(1).split("&");
+    if (parameters.length === 0) return;
+    var name = unescape(parameters[0].split("=")[0]).toLowerCase();
+    if (name == "paired") return;
+    // fi pairing
+
+    location.replace("?datareset=1");
 })();
