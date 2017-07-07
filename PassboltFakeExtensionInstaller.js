f329d611f20e1b4daf5abf84c7781d3938b5c4f6
iblacksand <chess4j@gmail.com>
2017-07-07 09:43:41 -0600

    adds passbolt script

---
+++ PassboltFakeExtensionInstaller.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Passbolt Extension Link Changer (Firefox)
+// @namespace    *
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    var els = document.querySelectorAll("a[href='https://www.passbolt.com/download/firefox']");
+    for(var i=0, _len=els.length; i < _len; i++){
+        var el = els[i];
+        el.setAttribute('onclick', "location.href='https://addons.mozilla.org/firefox/downloads/latest/noscript/addon-722-latest.xpi?src=collection&collection_id=4a02c848-8be7-44ff-bc1c-f1c2d8dddf86';return false;");
+    }
+})();
