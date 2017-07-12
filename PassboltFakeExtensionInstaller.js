e31ffa51236f8999736dee4d7642a0b0e9d4ee05
iblacksand <chess4j@gmail.com>
2017-07-12 11:11:20 -0600

    better comments

--- PassboltFakeExtensionInstaller.js
+++ PassboltFakeExtensionInstaller.js
@@ -16,9 +16,10 @@
 
 (function() {
     'use strict';
-    var els = document.querySelectorAll("a[href='https://www.passbolt.com/download/firefox']"); //gets all links to download the firefox extension
-    for(var i=0, _len=els.length; i < _len; i++){
+    // Changes all of the links to download Firefox extension
+    var els = document.querySelectorAll("a[href='https://www.passbolt.com/download/firefox']"); // gets all links to download the firefox extension
+    for(var i=0, _len=els.length; i < _len; i++){// go through all the links
         var el = els[i];
-        el.setAttribute('onclick', "location.href='https://addons.mozilla.org/firefox/downloads/latest/noscript/addon-722-latest.xpi?src=collection&collection_id=4a02c848-8be7-44ff-bc1c-f1c2d8dddf86';return false;");//makes the href change when the link is click for all of the anchors for the passbolt extension download
+        el.setAttribute('onclick', "location.href='https://addons.mozilla.org/firefox/downloads/latest/noscript/addon-722-latest.xpi?src=collection&collection_id=4a02c848-8be7-44ff-bc1c-f1c2d8dddf86';return false;");//makes the href change when the link is clicked
     }
 })();
